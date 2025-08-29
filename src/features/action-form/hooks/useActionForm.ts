cea3e44e0605e960384f33e1e1f106b6f2c492ce
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-08-30 00:15:21 +0900

    Add Lint Test CI

--- src/features/action-form/hooks/useActionForm.ts
+++ src/features/action-form/hooks/useActionForm.ts
@@ -4,12 +4,17 @@ import { useState, useMemo } from 'react';
 import { ActionType } from '../types';
 import { getPlaceholder } from '@/utils/getPlaceholder';
 
+{/* Step1 ActionForm Hooks */}
 export function useActionForm() {
+  /// action type
   const [type, setType] = useState<ActionType | null>(null);
+  /// Input Form Values
   const [description, setDescription] = useState('');
+  /// Step1〜3
   const [step, setStep] = useState<1 | 2>(1);
 
   const placeholder = useMemo(() => getPlaceholder(type), [type]);
+  /// Button Able or Disable
   const canProceed = !!type && description.trim().length > 0;
 
   const goToStep2 = () => {
